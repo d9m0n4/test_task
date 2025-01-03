673515cd8bfbd7d93af985375f475c89393f0053
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2025-01-03 23:36:57 +0300

    Delete test_task directory

--- test_task/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from './Header/Header';
-export { Pagination } from './Pagination/Pagination';
-export { ProductCard } from './ProductCard/ProductCard';
-export { Search } from './Search/Search';
--- test_task/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { CreateProduct } from './CreateProduct/CreateProduct';
-export { EditProduct } from './EditProduct/EditProduct';
-export { ProductDetails } from './ProductDetails/ProductDetails';
-export { Products } from './Products/Products';
